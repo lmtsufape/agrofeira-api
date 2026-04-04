81d884bc3f8cba2b53b5992f8af25ef97e7204c0
Julio Neto <nettojulio@hotmail.com>
2026-04-03 22:29:06 -0300

    refact(maven): clean and lean

---
+++ src/main/kotlin/br/edu/ufape/agrofeira/AgroFeiraApplication.kt
@@ -0,0 +1,11 @@
+package br.edu.ufape.agrofeira
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AgroFeiraApplication
+
+fun main(args: Array<String>) {
+	runApplication<AgroFeiraApplication>(*args)
+}
---
+++ src/test/kotlin/br/edu/ufape/agrofeira/AgroFeiraApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.edu.ufape.agrofeira
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AgroFeiraApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
